d32569ded3806cafe84ae16fac0b942261bf182f
BPanther <bpanther_ts@hotmail.com>
2013-02-26 19:27:59 +0100

    fix channelist.cpp

--- src/gui/channellist.cpp
+++ src/gui/channellist.cpp
@@ -491,12 +491,15 @@ void CChannelList::calcSize()
 		width = full_width;
 	}
 
-	height = h_max ((frameBuffer->getScreenHeight() / 20 * 18), 0);
+	height = h_max ((frameBuffer->getScreenHeight() / 20 * 17), 0);
 
 	x = frameBuffer->getScreenX() + (frameBuffer->getScreenWidth() - full_width) / 2;
 	if (x < ConnectLineBox_Width)
 		x = ConnectLineBox_Width;
-	y = frameBuffer->getScreenY();
+	if (g_settings.channellist_additional != 0)
+		y = frameBuffer->getScreenY() - 16;
+	else
+		y = frameBuffer->getScreenY() - 8;
 
 	CVFD::getInstance()->setMode(CVFD::MODE_MENU_UTF8, name.c_str());
 
