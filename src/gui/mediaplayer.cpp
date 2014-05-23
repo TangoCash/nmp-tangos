3f4a9307cfdf788300c2c7a450e8d2778a2ea9d6
TangoCash <eric@loxat.de>
2014-05-23 21:12:51 +0200

    small fix (thx udog)

--- src/gui/mediaplayer.cpp
+++ src/gui/mediaplayer.cpp
@@ -176,6 +176,15 @@ int CMediaPlayerMenu::initMenuMedia(CMenuWidget *m, CPersonalizeGui *p)
 		const char* inet_btn = usage_mode == MODE_AUDIO ? "" : NEUTRINO_ICON_BUTTON_GREEN;
 		fw_inet = new CMenuForwarder(LOCALE_INETRADIO_NAME, true, NULL, this, "inetplayer", inet_rc, inet_btn);
 		fw_inet->setHint(NEUTRINO_ICON_HINT_INET_RADIO, LOCALE_MENU_HINT_INET_RADIO);
+
+		//shairport
+		if (!access("/etc/init.d/shairport", X_OK)) {
+				neutrino_msg_t shairport_rc = usage_mode == MODE_AUDIO ? CRCInput::RC_blue : CRCInput::RC_nokey;
+				const char* shairport_btn = usage_mode == MODE_AUDIO ? NEUTRINO_ICON_BUTTON_BLUE : "";
+				fw_shairport = new CMenuForwarder(LOCALE_MAINMENU_SHAIRPORT, true, NULL, this, "shairport", shairport_rc, shairport_btn);
+				fw_shairport->setHint(NEUTRINO_ICON_HINT_INET_RADIO, LOCALE_MENU_HINT_SHAIRPORT);
+		}
+
 	}
 
 	if (usage_mode == MODE_DEFAULT)
